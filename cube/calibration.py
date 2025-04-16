bd3befe920764ac1a758d90dea6131e0aea9decd
rafguevara14 <66582348+rafguevara14@users.noreply.github.com>
2025-04-15 18:27:51 -0700

    add initial optimization pipeline

--- cube/calibration.py
+++ cube/calibration.py
@@ -4,7 +4,10 @@ import numpy as np
 import colorsys
 import time
 import drjit as dr
+import json
 
+
+    
 start = time.time()
 
 mi.set_variant('scalar_rgb')
@@ -12,207 +15,73 @@ mi.set_variant('scalar_rgb')
 def light_grid(face, n1, n2, const_val):
     positions = []
     if face == "top":
-        # Top face
         xs = np.linspace(-4, 4, n1)
         zs = np.linspace(-4, 4, n2)
         for x in xs:
             for z in zs:
-                positions.append([x, const_val, z])
+                positions.append((face, [x, const_val, z]))
     elif face == "back":
-        # Back face
         xs = np.linspace(-4, 4, n1)
         ys = np.linspace(-4, 4, n2)
         for x in xs:
             for y in ys:
-                positions.append([x, y, const_val])
-    elif face == "left":
-        # Left face
+                positions.append((face, [x, y, const_val]))
+    elif face == "left" or face == "right":
         ys = np.linspace(-4, 4, n1)
         zs = np.linspace(-4, 4, n2)
         for y in ys:
             for z in zs:
-                positions.append([const_val, y, z])
-    elif face == "right":
-        # Right face
-        ys = np.linspace(-4, 4, n1)
-        zs = np.linspace(-4, 4, n2)
-        for y in ys:
-            for z in zs:
-                positions.append([const_val, y, z])
+                positions.append((face, [const_val, y, z]))
+
     return positions
 
 
-# 10 x 15 = 150 lights
+def get_light_color(face, config_name):
+    with open('color_configs.json') as f:
+        color_configs = json.load(f)
+
+    if config_name not in color_configs:
+        raise ValueError(f"Configuration '{config_name}' not found in color_configs.json")
+    config = color_configs[config_name]
+
+    color = config.get(face, [0, 0, 0])
+
+    return color
+
+configs = json.load(open("color_configs.json"))
+print(configs.keys())
+
+
+# 10 x 15 = 150 lights per face
+left_pos  = light_grid("left",  10, 15, -4.5)  
 top_pos   = light_grid("top",   10, 15,  4.5)   
 back_pos  = light_grid("back",  10, 15, -4.5)  
-left_pos  = light_grid("left",  10, 15, -4.5)  
 right_pos = light_grid("right", 10, 15,  4.5)   
 
-# 12 x 12 = 144 lights
-# top_pos   = light_grid("top",   12, 12,  4.5)   
-# back_pos  = light_grid("back",  12, 12, -4.5)  
-# left_pos  = light_grid("left",  12, 12, -4.5)  
-# right_pos = light_grid("right", 12, 12,  4.5)   
-
-all_pos = top_pos + back_pos + left_pos + right_pos
-print("Total light emitters:", len(all_pos))  
+# all_pos = top_pos + back_pos + left_pos + right_pos
+all_pos = left_pos + top_pos + back_pos + right_pos
 
 
-
-def rainbow(i, total):
-    hue = i / total
-    saturation = 1.0
-    value = 1.0
-    r, g, b = colorsys.hsv_to_rgb(hue, saturation, value)
-    # return [r * 1.5, g * 1.5, b * 1.5]
-    return [r, g, b]
-
-# Suppose you already generated 'all_positions' for 600 lights
-# We'll just assume you have them in a list named all_positions.
-# Build emitter definitions, each with a unique color.
-emitters = {}
 num_emitters = len(all_pos)
-for i, pos in enumerate(all_pos):
-    color = rainbow(i, num_emitters) 
+print(f"Number of emitters: {num_emitters}")
+emitters = {}
+
+CONFIG = "RGB"
+
+for i, (face, pos) in enumerate(all_pos):
+    color = get_light_color(face, CONFIG)
     emitters[f"light_{i}"] = {
         "type": "point",
         "position": pos,
         "intensity": {
             "type": "rgb",
-            "value": color  # Use the rainbow function's RGB output
+            "value": color
         }
-
-        # "center": pos,
-        # "radius": 0.05,
-        # "emitter": {
-        #     "type": "area",
-        #     "radiance": {"type": "rgb", "value": color}
-        # },
-        # Optional: a diffuse BSDF so you see the spheres themselves
-        # "bsdf": {
-        #     "type": "diffuse",
-        #     "reflectance": {"type": "rgb", "value": [1, 1, 1]}
-        # }
     }
     
-
-
-scene_dict = {
-    "type": "scene",
-    "integrator": {"type": "path"},
-    "sensor": {
-        "type": "perspective",
-        "sampler": {
-            "type": "independent",
-            "sample_count": 512
-        },
-        "to_world": mi.ScalarTransform4f().look_at(
-            origin=[0, 0, 18],
-            target=[0, 0, 0],
-            up=[0, 1, 0]
-        ),
-        "film": {
-            "type": "hdrfilm",
-            "width": 800,
-            "height": 600,
-            "pixel_format": "rgb"
-        },
-    },
-    "env": {
-        "type": "constant",
-        "radiance": {"type": "rgb", "value": [0.01, 0.01, 0.01]}
-    },
-    "cube-back":{
-        "type": "obj",
-        "filename": "cube.obj",
-        "to_world": mi.ScalarTransform4f()
-            .translate([0, 0, -10])  
-        , 
-        "bsdf": {
-            "type": "twosided",
-            "bsdf": {
-                "type": "diffuse",
-                "reflectance": {"type": "rgb", "value": [0.2, 0.2, 0.2]}
-            }
-        }
-    },
-
-    "cube-top":{
-        "type": "obj",
-        "filename": "cube.obj",
-        "to_world": mi.ScalarTransform4f()
-            .translate([0, 10, 0])  
-        ,  
-        "bsdf": {
-            "type": "twosided",
-            "bsdf": {
-                "type": "diffuse",
-                "reflectance": {"type": "rgb", "value": [0.2, 0.2, 0.2]}
-            }
-        }
-    }, 
-    "cube-left":{
-        "type": "obj",
-        "filename": "cube.obj",
-        "to_world": mi.ScalarTransform4f()
-            .translate([10,0, 0]) 
-        ,  
-        "bsdf": {
-            "type": "twosided",
-            "bsdf": {
-                "type": "diffuse",
-                "reflectance": {"type": "rgb", "value": [0.2, 0.2, 0.2]}
-            }
-        }
-    }, 
-    "cube-right":{
-        "type": "obj",
-        "filename": "cube.obj",
-        "to_world": mi.ScalarTransform4f()
-            .translate([-10, 0, 0]) 
-        , 
-        "bsdf": {
-            "type": "twosided",
-            "bsdf": {
-                "type": "diffuse",
-                "reflectance": {"type": "rgb", "value": [0.2, 0.2, 0.2]}
-            }
-        }
-    }, 
-    "cube-bottom":{
-        "type": "obj",
-        "filename": "cube.obj",
-        "to_world": mi.ScalarTransform4f()
-            .translate([0, -10, 0])  
-        , 
-        "bsdf": {
-            "type": "twosided",
-            "bsdf": {
-                "type": "diffuse",
-                "reflectance": {"type": "rgb", "value": [0.2, 0.2, 0.2]}
-            }
-        }
-    },
-    "center_sphere": {
-        "type": "sphere",
-        "to_world": mi.ScalarTransform4f().translate([0, 0, 0]).scale(2),  # Adjust position & size
-        "bsdf": {
-            "type": "diffuse",
-            # "reflectance": {
-            #     "type": "rgb",
-            #     "value": [0.5, 0.5, 0.5]
-            # }
-            "reflectance": {
-                "type": "checkerboard",
-                "color0": {"type": "rgb", "value": [0.8, 0.8, 0.8]},
-                "color1": {"type": "rgb", "value": [0.2, 0.2, 0.2]},
-                "to_uv": mi.ScalarTransform4f().scale([5, 5, 5])
-
-
-            }
-        }
-    }
-}
+    
+for key, val in emitters['light_0'].items():
+    print( f"{key}: {val}")
 
 def idx_to_face(idx):
     if idx < 150:
@@ -225,15 +94,18 @@ def idx_to_face(idx):
         return "right"
     else:
         return None
-
-def update_emitters(emitters, translation, scale, orientation, faces_on = ["left", "top", "back", "right"]):
+    
+def get_transform(translation, scale, orientation, faces_on = ["left", "top", "back", "right"]):
     x, y, z = translation
+
     s = scale
+    if isinstance(scale, mi.Point1f):
+        s = scale[0][0]
     x_r, y_r, z_r = orientation
 
     transform = (
-        mi.ScalarTransform4f()
-        .translate([x, y, z])
+        mi.Transform4f()
+        .translate(translation)
         .rotate([1, 0, 0], x_r)
         .rotate([0, 1, 0], y_r)
         .rotate([0, 0, 1], z_r)
@@ -247,117 +119,295 @@ def update_emitters(emitters, translation, scale, orientation, faces_on = ["left
 
     # return
 
-    updated_emitters = {}
+    return transform
 
-    for name, light in emitters.items():
-        idx = int(name.split("_")[1])
-        face = idx_to_face(idx)
-        orig_pos = light["position"]
-        new_pos = transform @ mi.Point3f(orig_pos)
 
-        if face in faces_on: 
-            updated_emitters[name] = {
-                **light,
-                "position": new_pos
+#    updated_emitters = {}
+#
+#    for name, light in emitters.items():
+#        idx = int(name.split("_")[1])
+#        face = idx_to_face(idx)
+#        orig_pos = light["position"]
+#        new_pos = transform @ mi.Point3f(orig_pos)
+#
+#        if face in faces_on: 
+#            updated_emitters[name] = {
+#                **light,
+#                "position": new_pos
+#            }
+#        else:
+#            updated_emitters[name] = {
+#                **light,
+#                "position": new_pos,
+#                "intensity": {
+#                    "type": "rgb",
+#                    "value": [0, 0, 0]  # turn off the light
+#                }
+#            }
+#
+#    return updated_emitters
+
+
+
+scene_dict = {
+    "type": "scene",
+    "integrator": {"type": "path"},
+    "sensor": {
+        "type": "perspective",
+        "sampler": {"type": "independent", "sample_count": 512},
+        "to_world": mi.ScalarTransform4f().look_at(
+            origin=[0, -2, 19], target=[0, -5, 0], up=[0, 1, 0]
+        ),
+        "film": {
+            "type": "hdrfilm",
+            "width": 800,
+            "height": 600,
+            "pixel_format": "rgb"
+        },
+    },
+    "env": {
+        "type": "constant",
+        "radiance": {"type": "rgb", "value": [0.01, 0.01, 0.01]}
+    },
+    "checkerboard": {
+        "type": "rectangle",
+        "to_world": mi.ScalarTransform4f()
+            .translate([0, -5, 0])
+            .rotate([1, 0, 0], -90)
+            .scale([8, 8, 1]),
+        "bsdf": {
+            "type": "diffuse",
+            "reflectance": {
+                "type": "checkerboard",
+                "color0": {"type": "rgb", "value": [1.0, 1.0, 1.0]},
+                "color1": {"type": "rgb", "value": [0.0, 0.0, 0.0]},
+                "to_uv": mi.Transform4f().scale(mi.Vector3f(10, 10, 1))
             }
-        else:
-            updated_emitters[name] = {
-                **light,
-                "position": new_pos,
-                "intensity": {
-                    "type": "rgb",
-                    "value": [0, 0, 0]  # turn off the light
+        }
+    }
+}
+
+
+def render_scene(emitters, config="RGB", save_path=None):
+    scene_dict = {
+        "type": "scene",
+        "integrator": {"type": "path"},
+        "sensor": {
+            "type": "perspective",
+            "sampler": {"type": "independent", "sample_count": 512},
+            "to_world": mi.ScalarTransform4f().look_at(
+                origin=[0, -2, 19], target=[0, -5, 0], up=[0, 1, 0]
+            ),
+            "film": {
+                "type": "hdrfilm",
+                "width": 800,
+                "height": 600,
+                "pixel_format": "rgb"
+            },
+        },
+        "env": {
+            "type": "constant",
+            "radiance": {"type": "rgb", "value": [0.01, 0.01, 0.01]}
+        },
+        "checkerboard": {
+            "type": "rectangle",
+            "to_world": mi.ScalarTransform4f()
+                .translate([0, -5, 0])
+                .rotate([1, 0, 0], -90)
+                .scale([8, 8, 1]),
+            "bsdf": {
+                "type": "diffuse",
+                "reflectance": {
+                    "type": "checkerboard",
+                    "color0": {"type": "rgb", "value": [1.0, 1.0, 1.0]},
+                    "color1": {"type": "rgb", "value": [0.0, 0.0, 0.0]},
+                    "to_uv": mi.Transform4f().scale(mi.Vector3f(10, 10, 1))
                 }
             }
+        }
+    }
 
-    return updated_emitters
+    scene_dict.update(emitters)
+
+    scene = mi.load_dict(scene_dict)
+    img = mi.render(scene)
+    bitmap = mi.util.convert_to_bitmap(img)
+
+    if save_path:
+        plt.imshow(bitmap)
+        plt.axis('off')
+        plt.savefig(save_path, dpi=300, bbox_inches='tight')
+        plt.show()
+    else:
+        plt.imshow(bitmap)
+        plt.axis('off')
+        plt.show()
 
 
+def compare_scenes(real_img, virtual_img):
 
+    mi.set_variant('scalar_rgb')
+    start = time.time()
 
+    f, axarr = plt.subplots(1,2) 
+
+    # use the created array to output your multiple images. In this case I have stacked 4 images vertically
+    axarr[0].imshow(mi.util.convert_to_bitmap(real_img))
+    axarr[1].imshow(mi.util.convert_to_bitmap(virtual_img))
+
+    print('Comparing virtual and real scenes ({} s)'.format(time.time()-start))
+    plt.show()
+    mi.set_variant('llvm_ad_rgb')
 
 
 def take_picture(scene):
-    return mi.render(scene, spp=8)
+    # TODO: Change lights
+    mi.set_variant('scalar_rgb')
+    ret = mi.render(scene, spp=8)
+    mi.set_variant('llvm_ad_rgb')
+    return ret
 
+def render(scene):
+    mi.set_variant('scalar_rgb')
+    ret = mi.render(scene, spp=8)
+    mi.set_variant('llvm_ad_rgb')
+    return ret
     
 def error_function(image, image_ref):
-    return dr.mean(dr.sqr(image-image_ref))
+    assert type(image) == type(image_ref)
+    return dr.mean(dr.square(image-image_ref))
 
-def update_virtual_lights(opt):
-    T = mi.Transform4f.translate(opt['translation'])
+def update_virtual_lights(scene, opt):
 
-    new_light_positions = T 
+    params = mi.traverse(scene)
+
+    T = get_transform(
+        mi.Point3f(opt['translation']),
+        mi.Point1f(opt['scale']),
+        mi.Point3f(opt['orientation']),
+    )
+
+
+    # Apply transform to each light
+    for i in range(600):
+        key = f'light_{i}.position'
+        x,y,z = T @ params[key]
+        params[key] = dr.ravel([x,y,z])
+
+    params.update()
+
+def track_losses(epochs, losses):
+
+    ####### Matplotlib Interactive mode 
+    plt.ion()
+    ########### Sub plots ############
+    figure, ax = plt.subplots(figsize=(10, 8))
+    line, = ax.plot(epochs, losses)
+    ############ Title ###############
+    plt.title('Vectors', fontsize=20)
+    # setting x-axis label and y-axis label
+    plt.xlabel('Epoch')
+    plt.ylabel('MSE Loss')
+    
+    return figure, line
+
+
 
 def test_optimizer():
 
+    # Initialize scenes
     real_dict = scene_dict.copy()
     virtual_dict = scene_dict.copy()
     
     real_emitters = emitters.copy()
-    virtual_emitters = update_emitters(emitters, [10, 21, 4], 4, [0,0,0])
+    virtual_emitters = emitters.copy()
 
     real_dict.update(real_emitters)
     virtual_dict.update(virtual_emitters)
 
-    virtual_scene = mi.load_dict(scene_dict)
+    virtual_scene = mi.load_dict(virtual_dict)
     real_scene = mi.load_dict(real_dict)
 
-    virtual_params = mi.traverse(virtual_scene)
+    picture = take_picture(real_scene)
+    virtual_img = render(virtual_scene)
+    compare_scenes(picture, virtual_img)
+    t = dict()
+    t['translation'] =  mi.Point3f(0.0,0.0,0.0)
+    t['orientation'] =  mi.Point3f(90.0,0.0,0.0)
+    t['scale'] =  mi.Point1f(1.0)
+    update_virtual_lights(virtual_scene, t)
+    virtual_img = render(virtual_scene)
+    compare_scenes(picture, virtual_img)
 
-
+    print('Initializing optimizer...')
     mi.set_variant('llvm_ad_rgb')
-    import pdb; pdb.set_trace()
-    img = mi.render(virtual_scene)
-    mi.Scene
-    plt.imshow(mi.util.convert_to_bitmap(img))
-    plt.axis('off')
-    plt.show()
 
     opt = mi.ad.Adam(
-        lr=0.25
+        lr=0.05
     )
-
-    # camera parameters TODO: should be a separate optimization
-    # opt['angle'] =  mi.Point3f(0.0,0.0,0.0)
-    # opt['translation'] =  mi.Point1f(1.0)
-
-
     # cube parameters 
+    # Misalign virtual scene for testing
     opt['translation'] =  mi.Point3f(0.0,0.0,0.0)
     opt['orientation'] =  mi.Point3f(0.0,0.0,0.0)
     opt['scale'] =  mi.Point1f(1.0)
 
+    dr.enable_grad(opt['translation'])
+    dr.enable_grad(opt['orientation'])
+    dr.enable_grad(opt['scale'])
+    dr.enable_grad(virtual_img)
+
+    import pdb; pdb.set_trace()
 
     results = []
-    import pdb; pdb.set_trace()
     for light_setup in range(1):
         
-        # TODO: Change lights
-        print(light_setup)
         picture = take_picture(real_scene)
-        print(light_setup)
 
-        update_emitters(opt['translation'], opt['scale'], opt['orientation'])
+        epochs = np.array([0])
+        losses = np.array([0])
 
+        #figure, line = track_losses(epochs, losses)
         # Optimization Loop
-        for _ in range(50):
+        for epoch in range(5):
 
-            virtual_render = mi.render(virtual_scene, virtual_params, spp=8)
-            loss = error_function(virtual_render, picture)
-            print(loss)
-            dr.backward(loss)
-            opt.step()
+            print('Epoch: ', epoch)
 
             # clamp if necessarry
             # smallest scale, upper bound
-            virtual_emitters = update_emitters(virtual_emitters, [10, 21, 4], 4, [0,0,0])
-            virtual_params.update(virtual_emitters)
+            update_virtual_lights(virtual_scene, opt)
+            
+
+            virtual_img = render(virtual_scene)
+            compare_scenes(picture, virtual_img)
+            
+
+            loss = error_function(virtual_img, picture)
+            dr.backward(loss, flags=dr.ADFlag.Default | dr.ADFlag.AllowNoGrad)
+            print(opt['translation'])
+            print(opt['orientation'])
+            print(opt['scale'])
+            #opt.step()
+            print(loss)
+            
+            # Update graph
+            #epochs = np.append(epochs, epoch)
+            #losses = np.append(losses, loss)
+
+            #line.set_xdata(epochs)
+            #line.set_ydata(losses)
+            #plt.xlim([epochs.min()-1, epochs.max()+1])
+            #plt.ylim([losses.min()-0.01, losses.max()+0.01])
+            #figure.canvas.draw()
+            ## This will run the GUI event loop until all 
+            ## UI events currently waiting have been processed
+            #figure.canvas.flush_events()
+            #time.sleep(0.1)
+            
 
         # Finished optimizing against one light setup
         results.append((loss, opt))
 
-    parameters = next(sorted(results, key=lambda x: x[0]))[1]
+    parameters = sorted(results, key=lambda x: x[0])[0][1]
     # OR take the average
     #parameters = next(sorted(results, key=lambda x: x[0]))[1]
 
@@ -365,9 +415,22 @@ def test_optimizer():
     print(parameters)
 
 
+if __name__ == '__main__':
+
+    scene = mi.load_dict(scene_dict)
+    x = mi.traverse(scene)
+    # img = mi.render(scene)
+    # plt.imshow(mi.util.convert_to_bitmap(img))
+    # plt.axis('off')
+    # plt.show()
+
+    test_optimizer()
+
+    print(time.time()-start, "s")
 
 
-#test_optimizer()
-print(time.time()-start, "s")
 
-img = mi.render(mi.load_dict(scene_dict))
+
+    # camera parameters TODO: should be a separate optimization
+    # opt['angle'] =  mi.Point3f(0.0,0.0,0.0)
+    # opt['translation'] =  mi.Point1f(1.0)
